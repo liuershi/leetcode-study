7dd1756b5d63fa459ef83e017cb0cf5137a3aa6d
liuershi <liuershixixi@gmail.com>
2022-08-01 18:20:43 +0800

    [update] 反翻转链表基于迭代方式优化空间复杂度

--- src/main/java/com/leetcode/bytedance/Offer24.java
+++ src/main/java/com/leetcode/bytedance/Offer24.java
@@ -14,6 +14,25 @@ public class Offer24 {
     }
 
     public ListNode reverseList(ListNode head) {
+//        return method1(head);
+        // 基于迭代实现，时间复杂度为O(N)，空间复杂度为O(1)，相对递归的方式少了递归调用的开销
+        if (head == null || head.next == null) return null;
+        ListNode prev = null, next = head.next;
+        while (next != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+    /**
+     * 基于递归实现
+     * @param head
+     * @return
+     */
+    private ListNode method1(ListNode head) {
         if (head == null) return null;
         return helper(head, null);
     }
