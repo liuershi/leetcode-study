1c37500d6e231c01ff403ad1b52eaeec35a32a2d
张威151 <zhangwei151@hikvision.com.cn>
2022-10-12 09:03:42 +0800

    [update] 10.11优化代码时间复杂度

--- src/main/java/com/leetcode/bytedance/Demo1790.java
+++ src/main/java/com/leetcode/bytedance/Demo1790.java
@@ -8,6 +8,27 @@ package com.leetcode.bytedance;
 public class Demo1790 {
 
     public boolean areAlmostEqual(String s1, String s2) {
+//        return method1(s1, s2);
+
+        // 方式二：时间复杂度：O(N) 空间复杂度：O(1)
+        boolean exchange = false; // 表示是否交换过
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (exchange) return false;
+                int index = i + 1;
+                while (index < s1.length() && s1.charAt(index) == s2.charAt(index)) {
+                    index++;
+                }
+                if (index == s1.length()) return false;
+                // 确认两个位置的字符是否能否交换
+                if (s1.charAt(i) != s2.charAt(index) || s1.charAt(index) != s2.charAt(i)) return false;
+                exchange = true;
+                i = index;
+            }
+        }
+        return true;
+    }
+    private boolean method1(String s1, String s2) {
         // 思路：
         // 1.使用数组记录判断两个字符串的字符是否相等
         // 2.判断相同下标的字符数是否小于2
